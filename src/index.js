4ed46859bb2d0346a166bce02495f7c7dc09041f
Nachshon <nahshonrm@gmail.com>
2020-06-24 22:52:43 +0300

    #28__24.06.20__22-50_refactoring

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import runWelcome from './cli.js';
 
 const numberOfGameRounds = 3;
 
-export default (task, getData) => {
+export default (question, getData) => {
   const userName = runWelcome();
-  console.log(task);
+  console.log(question);
 
   // transmission of the question and verification of the answer
   for (let i = 0; i < numberOfGameRounds; i += 1) {
-    const [taskData, correctAnswer] = getData();
-    console.log(`Question: ${taskData}`);
+    const [questionData, correctAnswer] = getData();
+    console.log(`Question: ${questionData}`);
     const userResponse = readlineSync.question('Your answer:');
 
     if (userResponse.toString() !== correctAnswer.toString()) {
       console.log('------------------------------------------------\n'
-        + `Question: ${taskData}\nYour answer: ${userResponse}\n`
+        + `Question: ${questionData}\nYour answer: ${userResponse}\n`
         + `${userResponse} is wrong answer ;(. Correct answer was ${correctAnswer}.\n`
         + `Let's try again, ${userName}!`);
       break;
